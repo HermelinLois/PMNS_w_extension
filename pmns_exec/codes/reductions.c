206711214653341bf5d103fe84667d2e1b8567db
Me <hermelin.lois06@gmail.com>
2026-05-04 10:33:58 +0200

    clean up reductions.c file

--- pmns_exec/codes/reductions.c
+++ pmns_exec/codes/reductions.c
@@ -2,21 +2,6 @@
 # include <stdio.h>
 # include <gmp.h>
 
-//static mpz_t PHI;
-//static int PHI_ready = 0;
-//static const mp_limb_t phi_limbs[2] = {0ULL, 1ULL};
-
-
-//static inline void ensure_PHI() {
-//    if (PHI_ready)
-//        return;
-//
-//    mpz_init(PHI);
-//    mpz_import(PHI, 2, -1, sizeof(mp_limb_t), 0, 0, phi_limbs);
-//    PHI_ready = 1;
-//}
-
-
 #define DEFINE_SHIFT(NAME, OUT_TYPE, IN_TYPE)                                                   \
 static inline void NAME(OUT_TYPE out[DEGREE], const IN_TYPE polynomial[DEGREE], int n_shift) {  \
     for (int i = 0; i < DEGREE; i++) {                                                          \
@@ -55,8 +40,6 @@ void reduction_babai_int128(int64_t out[DEGREE], __int128 polynomial[DEGREE], co
 }
 
 
-
-
 void reduction_montgomery_int128(int64_t out[DEGREE], __int128 polynomial[DEGREE], const int64_t sublattice[DEGREE][DEGREE], const int64_t sublattice_inv[DEGREE][DEGREE]) {
     int64_t Q[DEGREE] = {0};
     __int128 T[DEGREE] = {0};
@@ -71,159 +54,6 @@ void reduction_montgomery_int128(int64_t out[DEGREE], __int128 polynomial[DEGREE
     for (int deg = 0; deg < DEGREE; deg++)
         out[deg] = (T[deg] + polynomial[deg]) >> 64;
 }
-
-
-
-
-// void reduction_montgomery_mpz(mpz_t out[DEGREE], mpz_t polynomial[DEGREE],  const int64_t sublattice[DEGREE][DEGREE], const int64_t sublattice_inv[DEGREE][DEGREE]) {
-//     mpz_t Q[DEGREE], T[DEGREE];
-
-//     for (int deg=0; deg<DEGREE; deg++){
-//         mpz_init_set_ui(Q[deg], 0);
-//         mpz_init_set_ui(T[deg], 0);
-//     }
-
-//     ensure_PHI();
-
-//     // compute (polynomial * sublattice_inv) % phi
-//     for (int j=0; j<DEGREE; j++){
-//         for (int i=0; i<DEGREE; i++){
-//             int64_t coeff = sublattice_inv[i][j];
-//             if (coeff == 0) continue;
-            
-//             int sign = (coeff < 0);
-//             uint64_t abs_coef = (sign ? (uint64_t) -coeff : (uint64_t) coeff);
-
-//             (sign ? mpz_submul_ui : mpz_addmul_ui)(Q[j], polynomial[i], abs_coef);
-//         }
-//         mpz_fdiv_r_2exp(Q[j], Q[j], PHI_POW);
-        
-//         if (mpz_tstbit(Q[j], PHI_POW - 1))
-//             mpz_sub(Q[j], Q[j], PHI);
-//     }
-    
-//     // compute T = Q * sublattice            
-//     for (int i=0; i<DEGREE; i++){                           
-//         for (int j=0; j<DEGREE; j++){
-//             int64_t coeff = sublattice[i][j];
-//             if (coeff == 0) continue;
-            
-//             int sign = (coeff < 0);
-//             uint64_t abs_coef = (sign ? (uint64_t) -coeff : (uint64_t) coeff);
-
-//             (sign ? mpz_submul_ui : mpz_addmul_ui)(T[j], Q[i], abs_coef);
-//         }
-//         mpz_clear(Q[i]);
-//     }
-
-//     for (int deg=0; deg<DEGREE; deg++){
-//         mpz_add(out[deg], polynomial[deg], T[deg]);
-//         mpz_fdiv_q_2exp(out[deg], out[deg], PHI_POW);
-//         mpz_clear(T[deg]);
-//     }        
-// }
-
-
-
-#define SHOW 100
-
-void reduction_montgomery_mpn_test(int n_limbs, mp_limb_t out[DEGREE][n_limbs], mp_limb_t abs_polynomial[DEGREE][n_limbs], int coeffs_sign[DEGREE], const int64_t sublattice[DEGREE][DEGREE], const int64_t sublattice_inv[DEGREE][DEGREE]){
-    int ACC_SIZE = n_limbs + 2;
-    int64_t Q[DEGREE];
-    mp_limb_t T[DEGREE][ACC_SIZE];
-    int T_sign[DEGREE];
-
-    // compute Q[j] = sum_i sublattice_inv[i][j] * P[i] % phi
-    for (int j = 0; j < DEGREE; j++) {
-        mp_limb_t acc[ACC_SIZE];
-        mpn_zero(acc, ACC_SIZE);
-
-        for (int i=0; i<DEGREE; i++) {
-            int64_t mat_coeff = sublattice_inv[i][j];
-            if (mat_coeff == 0) continue;
-
-            int mat_sign = (mat_coeff < 0);
-            mp_limb_t coeff_abs = mat_sign ? -(uint64_t)mat_coeff : (uint64_t)mat_coeff;
-
-            mp_limb_t prod[ACC_SIZE];
-            prod[n_limbs] = mpn_mul_1(prod, abs_polynomial[i], n_limbs, coeff_abs);
-
-            if (mat_sign ^ coeffs_sign[i])
-                mpn_sub_n(acc, acc, prod, ACC_SIZE);
-            else
-                mpn_add_n(acc, acc, prod, ACC_SIZE);
-        }
-
-        Q[j] = (int64_t)acc[0];
-    }
-
-    // compute T[j] = sum_i sublattice[i][j] * Q[i]
-    for (int j = 0; j < DEGREE; j++) {
-        mpn_zero(T[j], ACC_SIZE);
-        T_sign[j] = 0;
-
-        for (int i = 0; i < DEGREE; i++) {
-            int64_t mat_coeff = sublattice[i][j];
-            int64_t Q_coeff   = Q[i];
-            if (mat_coeff == 0 || Q_coeff == 0) continue;
-
-            int mat_sign = (mat_coeff < 0);
-            int Q_sign = (Q_coeff < 0);
-            mp_limb_t mat_abs = mat_sign ? -(uint64_t)mat_coeff : (uint64_t)mat_coeff;
-            mp_limb_t Q_abs = Q_sign ? -(uint64_t)Q_coeff : (uint64_t)Q_coeff;
-            int sign_prod = mat_sign ^ Q_sign;
-
-            // use mpn_mul_1 to compute the product and handle the sign at the same time
-            unsigned __int128 prod128 = (unsigned __int128)mat_abs * Q_abs;
-            mp_limb_t tmp[ACC_SIZE];
-            mpn_zero(tmp, ACC_SIZE);
-            tmp[0] = (mp_limb_t)prod128;
-            tmp[1] = (mp_limb_t)(prod128 >> GMP_NUMB_BITS);
-
-            if (T_sign[j] == sign_prod) {
-                mpn_add_n(T[j], T[j], tmp, ACC_SIZE);
-            } else {
-                if (mpn_cmp(T[j], tmp, ACC_SIZE) >= 0) {
-                    mpn_sub_n(T[j], T[j], tmp, ACC_SIZE);
-                } else {
-                    mpn_sub_n(T[j], tmp, T[j], ACC_SIZE);
-                    T_sign[j] = sign_prod;
-                }
-            }
-        }
-    }
-
-    // compute out[j] = (P[j] + T[j]) / phi and handle the sign
-    for (int deg = 0; deg < DEGREE; deg++) {
-        mp_limb_t acc[ACC_SIZE];
-        mpn_zero(acc, ACC_SIZE);
-
-        mp_limb_t P_ext[ACC_SIZE];
-        mpn_copyi(P_ext, abs_polynomial[deg], n_limbs);
-        P_ext[n_limbs] = 0;
-
-        int sP = coeffs_sign[deg];
-        int sT = T_sign[deg];
-        int sign;
-
-        if (sP == sT) {
-            mpn_add_n(acc, P_ext, T[deg], ACC_SIZE);
-            sign = sP;
-        } else {
-            if (mpn_cmp(P_ext, T[deg], ACC_SIZE) >= 0) {
-                mpn_sub_n(acc, P_ext, T[deg], ACC_SIZE);
-                sign = sP;
-            } else {
-                mpn_sub_n(acc, T[deg], P_ext, ACC_SIZE);
-                sign = sT;
-            }
-        }
-
-        coeffs_sign[deg] = sign;
-        mpn_copyi(out[deg], acc + 1, n_limbs);
-    }
-}
-
 
 
 
